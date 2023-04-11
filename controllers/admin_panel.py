529dbb417c3004564e2e04f3cc8c13718c8da92a
KaiLey <kailey2084@gmail.com>
2023-04-11 23:22:58 +1000

    vip

--- controllers/admin_panel.py
+++ controllers/admin_panel.py
@@ -4,7 +4,11 @@ from utils import create_connection
 from models.admin_panel_model import get_albums_names, add_song, \
                                      get_album_id, \
                                      get_performers_names, \
-                                     add_album
+                                     add_album, \
+                                     get_performer_id, \
+                                     get_song_id, \
+                                     delete_song, \
+                                     add_performer
 
 
 @app.route('/admin_panel', methods=['GET', 'POST'])
@@ -26,8 +30,26 @@ def admin_panel():
 
         add_song(conn, song_name, album_id)
 
+    if request.values.get('delete_performer'):
+        performer_id = get_performer_id(conn, request.values.get('delete_performer'))[0][0]
+        album_id = get_album_id(conn, request.values.get('delete_values'))[0][0]
+        song_id = get_song_id(conn, request.values.get('delete_song'), album_id, performer_id)[0][0]
+
+        delete_song(conn, song_id)
+
+
     if request.values.get('added_album_name'):
-        pass
+        album_name = request.values.get('added_album_name')
+        performer_id = get_performer_id(conn, request.values.get('new_album_performer'))[0][0]
+        type_id = int(request.values.get('album_type'))
+
+        add_album(conn, album_name, performer_id, type_id)
+
+    
+    if request.values.get('added_perf_name'):
+        performer_name = request.values.get('added_perf_name')
+
+        add_performer(conn, performer_name)
 
 
     html = render_template(
--- models/admin_panel_model.py
+++ models/admin_panel_model.py
@@ -29,6 +29,38 @@ def get_album_id(conn, album_name):
     """
 
     return execute_read_query(conn, query)
+
+
+# def get_song_id(conn, song_name):
+#     query = f"""
+#         SELECT song_id
+#         FROM song
+#         WHERE song_name = '{song_name}'
+#     """
+
+#     return execute_read_query(conn, query)
+
+
+def get_song_id(conn, song_name, album_id, performer_id):
+    query = f"""
+        SELECT song_id
+        FROM song
+            JOIN album USING(album_id)
+            JOIN performer USING(performer_id)
+        WHERE song_name = '{song_name}' AND album_id = {album_id} AND performer_id = {performer_id}
+    """
+
+    return execute_read_query(conn, query)
+
+
+def delete_song(conn, song_id):
+    query = f"""
+        DELETE FROM song
+        WHERE song_id = {song_id}
+    """
+
+    execute_query(conn, query)
+
 # Songs -----------------------------------------------------------
 
 # Albums ----------------------------------------------------------
@@ -36,6 +68,7 @@ def get_performers_names(conn):
     query = """
         SELECT performer_name
         FROM performer
+        ORDER BY performer_name
     """
 
     return execute_read_query(conn, query)
@@ -50,9 +83,28 @@ def add_album(conn, album_name, performer_id, type_id):
     execute_query(conn, query)
 
 
+def get_performer_id(conn, performer_name):
+    query = f"""
+        SELECT performer_id
+        FROM performer
+        WHERE performer_name = '{performer_name}'
+    """
+
+    return execute_read_query(conn, query)
 # Albums ----------------------------------------------------------
 
 
+# Perf ----------------------------------------------------------
+
+def add_performer(conn, performer_name):
+    query = f"""
+        INSERT INTO performer (performer_name)
+        VALUES ('{performer_name}')
+    """
+
+    execute_query(conn, query)
+
+# Perf ----------------------------------------------------------
 
 
 
